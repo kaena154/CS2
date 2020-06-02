2f5bfe904bdd2f73d4a9d9bc6db0748fea1bb361
Sabrina Zhukova <kaena154@gmail.com>
2020-06-02 03:36:49 +0300

    lab2 is ready

---
+++ Lab2_2/Lab2_2/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Division division = new Division();
+            division.GetInputData();
+            division.Divide();
+            Console.WriteLine($"Fraction: {division.Fraction}\nRemainder: {division.Remainder}");
+
+            Console.ReadKey();
+        }
+    }
+    class Division
+    {
+        public int Divided { get; set; }
+        public int Divider { get; set; }
+        public List<int> BinRemainder { get; set; }
+        public List<int> BinDivisor { get; set; }
+        public List<int> BinQuotient { get; set; }
+        public int Fraction { get; set; }
+        public int Remainder { get; set; }
+
+        public void GetInputData()
+        {
+            BinQuotient = new List<int>();
+            int temp;
+            do
+            {
+                Console.Write("Enter divided: ");
+            }
+            while (!Int32.TryParse(Console.ReadLine(), out temp));
+            this.Divided = temp;
+            do
+            {
+                Console.Write("Enter divider: ");
+            }
+            while (!Int32.TryParse(Console.ReadLine(), out temp));
+            this.Divider = temp;
+            if (Divided < 0 && Divider < 0)
+            {
+                Divided = Math.Abs(Divided);
+                Divider = Math.Abs(Divider);
+            }
+
+            this.BinRemainder = GetNumbInBinary(this.Divided);
+            SupplementBits(this.BinRemainder, this.Divided, 64);
+            this.BinDivisor = GetNumbInBinary(this.Divider);
+            SupplementBits(this.BinDivisor, this.Divider, 32);
+        }
+        public List<int> GetNumbInBinary(int number)
+        {
+            int absNumb = Math.Abs(number);
+            List<int> result = new List<int>();
+            while (absNumb > 0)
+            {
+                result.Add(absNumb % 2);
+                absNumb /= 2;
+            }
+            result.Reverse();
+            if (number < 0)
+            {
+                Inverse(ref result);
+            }
+            return result;
+        }
+        public void Inverse(ref List<int> binaryNumb)
+        {
+            for (int i = 0; i < binaryNumb.Count; i++)
+            {
+                if (binaryNumb[i] == 0)
+                {
+                    binaryNumb[i] = 1;
+                }
+                else
+                {
+                    binaryNumb[i] = 0;
+                }
+            }
+            binaryNumb = AddOne(binaryNumb);
+        }
+        public List<int> AddOne(List<int> binaryNumb)
+        {
+            int mind = 1;
+            for (int i = binaryNumb.Count - 1; i >= 0; i--)
+            {
+                if (i == 0 && binaryNumb[i] == 1 && mind == 1)
+                {
+                    List<int> newBinaryNumb = new List<int>();
+                    newBinaryNumb.AddRange(new List<int>() { 1, 1 });
+                    newBinaryNumb.AddRange(binaryNumb);
+                    return newBinaryNumb;
+                }
+                else if (binaryNumb[i] == 0 && mind == 1)
+                {
+                    binaryNumb[i] = 1;
+                    mind = 0;
+                }
+                else if (binaryNumb[i] == 1 && mind == 1)
+                {
+                    binaryNumb[i] = 0;
+                }
+            }
+            return binaryNumb;
+        }
+        public void SupplementBits(List<int> binaryNumb, int number, int forSupplement)
+        {
+            int numberForSupplement = 0;
+            if (number < 0)
+            {
+                numberForSupplement = 1;
+            }
+            List<int> helper = new List<int>();
+            int countToSupplement = forSupplement - binaryNumb.Count;
+            for (int i = 0; i < countToSupplement; i++)
+            {
+                helper.Add(numberForSupplement);
+            }
+            helper.AddRange(binaryNumb);
+            binaryNumb.Clear();
+            binaryNumb.AddRange(helper);
+        }
+        public void Divide()
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                ShiftLeft(BinRemainder);
+                Output(BinRemainder, "\nDivided");
+                Output(BinDivisor, "\nDivisor");
+                Output(BinQuotient, "\nFraction");
+                Subtract(BinRemainder, BinDivisor);
+                Console.WriteLine();
+                if (BinRemainder[0] == 0)
+                {
+                    BinQuotient.Add(1);
+                }
+                else
+                {
+                    BinQuotient.Add(0);
+                    Add(BinRemainder, BinDivisor);
+                }
+            }
+            Fraction = GetNumbFromBinary(BinQuotient);
+            Remainder = GetNumbFromBinary(BinRemainder.Take(32).ToList());
+        }
+        public void Add(List<int> remainder, List<int> divisor)
+        {
+            int mind = 0;
+            for (int i = 31; i >= 0; i--)
+            {
+                if ((remainder[i] == 0 && divisor[i] == 1 && mind == 0) || (remainder[i] == 1 && divisor[i] == 0 && mind == 0) || (remainder[i] == 0 && divisor[i] == 0 && mind == 1))
+                {
+                    remainder[i] = 1;
+                    mind = 0;
+                }
+                else if ((remainder[i] == 0 && divisor[i] == 1 && mind == 1) || (remainder[i] == 1 && divisor[i] == 0 && mind == 1) || (remainder[i] == 1 && divisor[i] == 1 && mind == 0))
+                {
+                    remainder[i] = 0;
+                    mind = 1;
+                }
+                else if (remainder[i] == 0 && divisor[i] == 0 && mind == 0)
+                {
+                    remainder[i] = 0;
+                    mind = 0;
+                }
+                else
+                {
+                    remainder[i] = 1;
+                    mind = 1;
+                }
+            }
+        }
+        public void Output(List<int> binNumb, string what)
+        {
+            Console.Write(what + ": ");
+            for (int i = 0; i < binNumb.Count; i++)
+            {
+                Console.Write(binNumb[i]);
+            }
+            Console.Write("\t");
+        }
+        public int GetNumbFromBinary(List<int> binNumb)
+        {
+            int result = 0;
+            int power2 = 0;
+            for (int i = binNumb.Count - 1; i >= 0; i--)
+            {
+                if (binNumb[i] == 1)
+                {
+                    result += (int)Math.Pow(2, power2);
+                }
+                power2++;
+            }
+            return result;
+        }
+        public void ShiftLeft(List<int> remainder)
+        {
+            for (int i = 0; i < 63; i++)
+            {
+                remainder[i] = remainder[i + 1];
+            }
+            remainder[63] = 0;
+        }
+        public void Subtract(List<int> reduction, List<int> subtractor)
+        {
+            List<int> helperSubtract = new List<int>();
+            helperSubtract.AddRange(subtractor);
+            Inverse(ref helperSubtract);
+            Add(reduction, helperSubtract);
+        }
+    }
+}
---
+++ Lab2_3/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            IEEE754_Multiply floating_multiplier = new IEEE754_Multiply();
+            float firstNumber = 0, secondNumber = 0;
+            string first = "", second = ""; ;
+            Console.WriteLine("Multiplication of numbers");
+            do
+            {
+                try
+                {
+                    Console.Write("Enter first value: ");
+                    first = Console.ReadLine();
+                }
+                catch (FormatException exc)
+                {
+                    Console.WriteLine("Wrong format: " + exc);
+                }
+            } while (!float.TryParse(first, out firstNumber));
+            do
+            {
+                try
+                {
+                    Console.Write("Enter second value: ");
+                    second = Console.ReadLine();
+                }
+                catch (FormatException exc)
+                {
+                    Console.WriteLine("Wrong format: " + exc);
+                }
+            } while (!float.TryParse(second, out secondNumber));
+
+            float result = floating_multiplier.FloatingNumbersMultiplication(firstNumber, secondNumber);
+
+            Console.ReadKey();
+        }
+    }
+    class IEEE754_Multiply
+    {
+        public IEEE754_Multiply() { }
+
+        public string ConvertToPrint(string number, int bits_amount)
+        {
+            int zeros_amount = bits_amount - number.Length;
+            string zeros = "";
+            for (int i = 0; i < zeros_amount; i++)
+            {
+                zeros += "0";
+            }
+            return zeros + number;
+        }
+
+        public int FloatingBitsMask(float number)
+        {
+            byte[] bits = BitConverter.GetBytes(number);
+            int floatingResult = 0;
+            floatingResult |= bits[0]; 
+            floatingResult |= bits[1] << 8; 
+            floatingResult |= bits[2] << 16;
+            floatingResult |= bits[3] << 24; 
+            return floatingResult;
+        }
+
+        public float FloatingNumbersMultiplication(float first, float second)
+        {
+            long firstBits = FloatingBitsMask(first);
+            long secondBits = FloatingBitsMask(second);
+
+            int sign_1 = (int)((firstBits >> 31) & 1);
+            int sign_2 = (int)((secondBits >> 31) & 1);
+            int resultSign = 1 & (sign_1 + sign_2);
+            Console.WriteLine("\nDefinition of the sign:");
+            Console.WriteLine(sign_1 + " XOR " + sign_2 + " = " + resultSign);
+
+            Console.WriteLine("\nMultiplication :");
+            long mantissa_1 = firstBits & ((int)Math.Pow(2, 23) - 1);
+            Console.WriteLine("Mantissa of the first number:\n " + ConvertToPrint(Convert.ToString(mantissa_1, 2), 24)
+                + "\nХ (multiply by)");
+            long mantissa_2 = secondBits & ((int)Math.Pow(2, 23) - 1);
+            Console.WriteLine("Mantissa of the second number:\n " + ConvertToPrint(Convert.ToString(mantissa_2, 2), 24)
+                + "\n= (is equal to)");
+            long multiplicatedMantissa = ((1 << 23) | mantissa_1) * ((1 << 23) | mantissa_2);
+            Console.WriteLine("Mantissa resulting : " + ConvertToPrint(Convert.ToString(multiplicatedMantissa, 2), 48));
+
+            multiplicatedMantissa >>= 23;
+            int normalizer = ((1 << 24) & multiplicatedMantissa) > 0 ? 1 : 0;
+
+            Console.WriteLine();
+            if (normalizer > 0)
+            {
+                Console.WriteLine("Normalization");
+                multiplicatedMantissa >>= 1;
+                multiplicatedMantissa &= ~(1 << 23);
+            }
+            else
+            {
+                multiplicatedMantissa &= ~(3 << 23);
+            }
+            Console.WriteLine("The result: ");
+            Console.WriteLine(ConvertToPrint(Convert.ToString(multiplicatedMantissa, 2), 23));
+            Console.WriteLine();
+
+            int exponent_1 = (int)((firstBits >> 23) & 255),
+                exponent_2 = (int)((secondBits >> 23) & 255);
+            int resultExponent = exponent_1 + exponent_2 - 127 + normalizer;
+
+            Console.WriteLine();
+            Console.WriteLine("Calculation of the exponent of the result: ");
+            Console.WriteLine("The exponent of the first number: " + ConvertToPrint(Convert.ToString(exponent_1, 2), 8)
+                + "\nthat in decimal form: " + exponent_1);
+            Console.WriteLine("The exponent of the second number : " + ConvertToPrint(Convert.ToString(exponent_2, 2), 8)
+                + "\nthat in decimal form: " + exponent_2);
+            Console.WriteLine("The resulting exponent: ");
+            Console.Write(exponent_1 + " " + exponent_2 + " - 127 + " + normalizer + " = " + resultExponent
+                + "\nthat in binary representation: " + ConvertToPrint(Convert.ToString(resultExponent, 2), 8));
+
+            int resultFloatingMask = (int)multiplicatedMantissa;
+            resultFloatingMask |= resultExponent << 23;
+            resultFloatingMask |= resultSign << 31;
+
+            byte[] floatingBits = new byte[4];
+            floatingBits[0] = (byte)(resultFloatingMask & 255);
+            floatingBits[1] = (byte)((resultFloatingMask >> 8) & 255);
+            floatingBits[2] = (byte)((resultFloatingMask >> 16) & 255);
+            floatingBits[3] = (byte)((resultFloatingMask >> 24) & 255);
+
+            float floatingResult = BitConverter.ToSingle(floatingBits, 0);
+            Console.WriteLine("\n\nAnswer:\nBinary:\n"
+                + ConvertToPrint(Convert.ToString(resultFloatingMask, 2), 32)
+                + "\nDecimal: " + floatingResult);
+            return floatingResult;
+        }
+    }
+}
